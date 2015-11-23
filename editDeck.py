d7c25e0048a28187fd9b909a43e9224ae79e5351
Michael Ghizzi <michaelghizzi@gmail.com>
2015-11-22 16:28:38 -0800

    Create editDeck.py

---
+++ editDeck.py
@@ -0,0 +1,120 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+import tkinter as tk  # gives tk namespace
+
+#Edit Deck 
+
+def add_item():
+    listbox1.insert(tk.END, enter1.get())
+    
+def delete_item():
+    try:
+        # get selected line index
+        index = listbox1.curselection()[0]
+        listbox1.delete(index)
+    except IndexError:
+        pass
+ 
+def get_list(event):
+    # get selected line index
+    index = listbox1.curselection()[0]
+    # get the line's text
+    seltext = listbox1.get(index)
+    # delete previous text in enter1
+    enter1.delete(0, 50)
+    # now display the selected text
+    enter1.insert(0, seltext)
+    
+def set_list(event):
+    try:
+        index = listbox1.curselection()[0]
+        # delete old listbox line
+        listbox1.delete(index)
+    except IndexError:
+        index = tk.END
+    # insert edited item back into listbox1 at index
+    listbox1.insert(index, enter1.get())
+    
+def sort_list():
+    temp_list = list(listbox1.get(0, tk.END))
+    temp_list.sort(key=str.lower)
+    # delete contents of present listbox
+    listbox1.delete(0, tk.END)
+    # load listbox with sorted data
+    for item in temp_list:
+        listbox1.insert(tk.END, item)
+        
+def save_list():
+    # get a list of listbox lines
+    temp_list = list(listbox1.get(0, tk.END))
+    # add a trailing newline char to each line
+    temp_list = [flash + '\n' for flash in temp_list]
+    # give the file a different name
+    fout = open("flashCards.txt", "w")
+    fout.writelines(temp_list)
+    fout.close()
+
+# read the data file into a list
+fin = open("flashCards.txt", "r")
+flash_list = fin.readlines()
+fin.close()
+
+# strip the trailing newline char
+flash_list = [flash.rstrip() for flash in flash_list]
+ 
+root = tk.Tk()
+root.title("Edit Cards")
+
+# create the listbox (note that size is in characters)
+# listbox1 = tk.Listbox(root, width=120, height=30)
+# listbox1.grid(row=0, column=0)
+
+
+listbox1 = tk.Listbox(root, width=120, height=30)
+listbox1.grid(row=0, column=0)
+
+# listbox1 = tk.Listbox(root, width=60, height=30)
+# listbox1.grid(row=0, column=2)
+
+# listbox2 = tk.Listbox(root, width=20, height=20)
+# listbox2.grid(row=0, column=0)
+ 
+# create a vertical scrollbar to the right of the listbox
+yscroll = tk.Scrollbar(command=listbox1.yview, orient=tk.VERTICAL)
+yscroll.grid(row=0, column=1, sticky=tk.N+tk.S)
+listbox1.configure(yscrollcommand=yscroll.set)
+ 
+# use entry widget to display/edit selection
+enter1 = tk.Entry(root, width=50, bg='cyan')
+enter1.insert(0, 'Click on an item in the listbox')
+enter1.grid(row=2, column=0)
+
+# pressing the return key will update edited line
+enter1.bind('<Return>', set_list)
+
+# or double click left mouse button to update line
+enter1.bind('<Double-1>', set_list)
+
+# button to sort listbox
+button1 = tk.Button(root, text='Sort the listbox    ', command=sort_list)
+button1.grid(row=2, column=0, sticky=tk.W)
+
+# button to save the listbox's data lines to a file
+button2 = tk.Button(root, text='Save lines to file', command=save_list)
+button2.grid(row=3, column=0, sticky=tk.W)
+
+# button to add a line to the listbox
+button3 = tk.Button(root, text='Add entry text to listbox', command=add_item)
+button3.grid(row=2, column=0, sticky=tk.E)
+
+# button to delete a line from listbox
+button4 = tk.Button(root, text='Delete selected line     ', command=delete_item)
+button4.grid(row=3, column=0, sticky=tk.E)
+
+# load the listbox with data
+for item in flash_list:
+    listbox1.insert(tk.END, item)
+ 
+# left mouse click on a list item to display selection
+listbox1.bind('<ButtonRelease-1>', get_list)
+
+root.mainloop()
