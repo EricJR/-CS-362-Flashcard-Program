d9010a2af8733e91d5d153202e2501ce846e2b0e
Michael Ghizzi <michaelghizzi@gmail.com>
2015-11-29 16:03:02 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,29 +5,42 @@
 
 #I am getting the error: error: [Errno 2] No such file or directory: '/Library/Frameworks/Tcl.framework/Versions/8.5/Tcl'
 
-application_title = "Flash_Card_Program"
-main_python_file = "flashCards.py"
 
 import sys
 from cx_Freeze import setup, Executable
 
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-includes = ["atexit","re"]
-
 setup(
-      name = application_title,
-      version = "1.0",
-      description = "Sample cx_Freeze PyQt4 script",
-      options = {
-      "build_exe" : {
-      "includes" : includes,
-      "excludes": ['Tcl', 'ttk', 'tkinter', 'Tkinter'],
-      }
-      },
-      executables = [
-                     Executable(main_python_file, base = base)
-                     ]
-      )
+    name = "Flash_Card_Program",
+    version = "3",
+    description = "A flashcard program",
+    executables = [Executable("flashCards.py", base = "Win32GUI")])
+
+
+## ANOTHER EXAMPLE: 
+
+# application_title = "Flash_Card_Program"
+# main_python_file = "flashCards.py"
+
+# import sys
+# from cx_Freeze import setup, Executable
+
+# base = None
+# if sys.platform == "win32":
+#     base = "Win32GUI"
+
+# includes = ["atexit","re"]
+
+# setup(
+#       name = application_title,
+#       version = "1.0",
+#       description = "Sample cx_Freeze PyQt4 script",
+#       options = {
+#       "build_exe" : {
+#       "includes" : includes,
+#       "excludes": ['Tcl', 'ttk', 'tkinter', 'Tkinter'],
+#       }
+#       },
+#       executables = [
+#                      Executable(main_python_file, base = base)
+#                      ]
+#       )
