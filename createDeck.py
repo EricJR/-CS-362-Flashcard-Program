bd7af07ed6a9842034386988b80ca8da7ae29e79
rfernando89 <rfernando89@csu.fullerton.edu>
2015-12-01 01:17:36 -0800

    Update createDeck.py

--- createDeck.py
+++ createDeck.py
@@ -1,6 +1,7 @@
 #User enters deck name and deck description. They will then click on Save deck to save the deck information to a text file
 #Format of newly created deck in textfile will be @@@DeckName~~DeckDescription
 
+
 import os
 import sys
 import os.path
@@ -31,20 +32,20 @@ class createDeck(tk.Frame):
 
         # Labels for "Enter Deck Name" and "Enter Deck Description"
         Label(self, text="Enter Deck Name").grid(row=0)
-        self.e1 = tk.Entry(self, textvariable=self.usertext)
-        self.e1.grid(row=0, column=1)
+        e1 = tk.Entry(self, textvariable=self.usertext)
+        e1.grid(row=0, column=1)
 
         Label(self, text="Enter Deck Description").grid(row=1)
-        self.e2 = tk.Entry(self, textvariable=self.usertext2)
-        self.e2.grid(row=1, column=1)
+        e2 = tk.Entry(self, textvariable=self.usertext2)
+        e2.grid(row=1, column=1)
 
         # User needs to click save deck
-        self.button = Button(self, text="Save Deck", command=self.printMessage)
-        self.button.grid(row=2, column=1)
+        button = Button(self, text="Save Deck", command=self.printMessage)
+        button.grid(row=2, column=1)
 
     # Print deck to textfile. Display an OK messagebox if successful.
     def printMessage(self):
-        text_file = open("flashCards.txt", "a")
+        text_file = open("Flashcards.txt", "a")
         text_file.write("@@@" + self.usertext.get() + "~~" + self.usertext2.get() + "\n")
         text_file.close()
         #print("Writing to file...")
