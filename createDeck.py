0b5ac5e838d4cbf5ab873ad2d967a083ca632867
rfernando89 <rfernando89@csu.fullerton.edu>
2015-12-05 21:58:58 -0800

    Update createDeck.py

--- createDeck.py
+++ createDeck.py
@@ -74,27 +74,38 @@ class createDeck(tk.Frame):
         self.usertext2 = StringVar()
 
         # Labels for "Enter Deck Name" and "Enter Deck Description"
-        Label(self, text="Enter Deck Name").grid(row=0)
+        Label(self, text="Enter Deck Name").pack()
         self.e1 = tk.Entry(self, textvariable=self.usertext)
-        self.e1.grid(row=0, column=1)
+        self.e1.pack()
 
-        Label(self, text="Enter Deck Description").grid(row=1)
+        Label(self, text="Enter Deck Description").pack()
         self.e2 = tk.Entry(self, textvariable=self.usertext2)
-        self.e2.grid(row=1, column=1)
+        self.e2.pack()
 
         # User needs to click save deck
         button = Button(self, text="Save Deck", command=self.printContents)
-        button.grid(row=2, column=1)
+        button.pack()
 
+        self.status = Label(self,text="Create your Deck",bd=1,relief=SUNKEN,anchor=W)
+        self.status.pack(side=BOTTOM,fill=X)
 
     # Print deck to textfile. Display an OK messagebox if successful. Will check if user actually entered deck name and description before writing to file.
     def printContents(self):
+
+        self.status.destroy()
+
         if len(self.usertext.get()) == 0 and len(self.usertext2.get()) == 0:
-            tkinter.messagebox.showinfo("Error", "You need to enter a Deck Name and Description!")
+            #tkinter.messagebox.showinfo("Error", "You need to enter a Deck Name and Description!")
+            self.status = Label(self,text="Error: You need to enter a Deck Name and Description!",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
         elif len(self.usertext.get()) == 0:
-            tkinter.messagebox.showinfo("Error", "You need to enter a Deck Name!")
+            #tkinter.messagebox.showinfo("Error", "You need to enter a Deck Name!")
+            self.status = Label(self,text="Error: You need to enter a Deck Name!",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
         elif len(self.usertext2.get()) == 0:
-            tkinter.messagebox.showinfo("Error", "You need to enter a Deck Description!")
+            #tkinter.messagebox.showinfo("Error", "You need to enter a Deck Description!")
+            self.status = Label(self,text="Error: You need to enter a Deck Description!",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
         else:
             text_file = open("Flashcards.txt", "a")
             text_file.write("@@@" + self.usertext.get() + "~~" + self.usertext2.get() + "\n")
@@ -122,33 +133,45 @@ class addCards(tk.Frame):
         self.cardFront = StringVar()
         self.backCard = StringVar()
 
-        Label(self, text="Enter Question").grid(row=0)
+        Label(self, text="Enter Question").pack()
         self.e1 = tk.Entry(self, textvariable=self.cardFront)
-        self.e1.grid(row=0, column=1)
+        self.e1.pack()
 
-        Label(self, text="Enter Answer").grid(row=1)
+        Label(self, text="Enter Answer").pack()
         self.e2 = tk.Entry(self, textvariable=self.backCard)
-        self.e2.grid(row=1, column=1)
+        self.e2.pack()
 
         # User needs to click save deck
         button = Button(self, text="Save Card", command=self.printContents)
-        button.grid(row=2, column=1)
+        button.pack()
 
         button2 = Button(self, text="Go Back to Create Deck", command = self.goBack)
-        button2.grid(row=3,column=1)
+        button2.pack()
 
         #Exit button
         button3 = Button(self, text="Exit", command=self.quit)
-        button3.grid(row=4, column=1)
+        button3.pack()
+
+        self.status = Label(self,text="Add Cards to your Deck",bd=1,relief=SUNKEN,anchor=W)
+        self.status.pack(side=BOTTOM,fill=X)
 
     #Checks if user entered both front and back information. If they did it will write question and answer to text file.
     def printContents(self):
+
+        self.status.destroy()
+
         if len(self.cardFront.get()) == 0 and len(self.backCard.get()) == 0:
-            tkinter.messagebox.showinfo("Error", "You need to enter a Question and Answer!")
+            #tkinter.messagebox.showinfo("Error", "You need to enter a Question and Answer!")
+            self.status = Label(self,text="Error: You need to enter a Question and Answer",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
         elif len(self.cardFront.get()) == 0:
-            tkinter.messagebox.showinfo("Error", "You need to enter a Question!")
+            #tkinter.messagebox.showinfo("Error", "You need to enter a Question!")
+            self.status = Label(self,text="Error: You need to enter a Question!",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
         elif len(self.backCard.get()) == 0:
-            tkinter.messagebox.showinfo("Error", "You need to enter an Answer!")
+            #tkinter.messagebox.showinfo("Error", "You need to enter an Answer!")
+            self.status = Label(self,text="Error: You need to enter an Answer",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
         else:
             text_file = open("Flashcards.txt", "a")
             text_file.write(self.cardFront.get() + "~~" + self.backCard.get() + "\n")
@@ -157,7 +180,9 @@ class addCards(tk.Frame):
             self.e1.delete(0,END)
             self.e2.delete(0,END)
             #print("Writing to file...")
-            tkinter.messagebox.showinfo("Card Added!", "Successfully added FlashCard to your deck.")
+            #tkinter.messagebox.showinfo("Card Added!", "Successfully added FlashCard to your deck.")
+            self.status = Label(self,text="Card Added! Successfully added FlashCard your Deck!",bd=1,relief=SUNKEN,anchor=W)
+            self.status.pack(side=BOTTOM,fill=X)
 
     def quit(self):
         self.controller.quitProgram()
