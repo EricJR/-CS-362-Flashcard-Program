a65479d0a76075b114625e9030665235f64166f3
EricJR <ericjohnroe@gmail.com>
2015-12-06 15:55:54 -0800

    Changing to main.py
    Previously TERMINAL_IMPLEMENTATION.py
    
    This will be where we call everything from.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+#TODO: Import all files. Run the app.
+
+
+
+
+from flashcard_classes import *
 # Testing to make it work in Terminal.
 # Next steps: After this works, we can work on using tkinter with it.
 
-from flashcard_classes import Flashcard, Deck, FlashcardController, FileSystemStorage
+#from library.file_reader import FileSystemStorage
+#from library.flashcard_classes import *
 
 def main():
     controller = FlashcardController()
     file_sys = FileSystemStorage()
-    debug = True
+    debug = False
     
     # Testing the file system
     file_sys.read_from_file(controller)
     
     # Currently debugging and auto-creating some decks
+    """
     if (debug):
         title = "Eric's Deck"
         deck = "This is the description of Eric's Deck."
         controller.new_deck(title,deck)
-        controller.add_cards_to_deck(title)
+        controller = controller.add_cards_to_deck(title)
         title = "Deck Dos"
         desc = "Description Deck Dos, dawg."
         controller.new_deck(title, desc)
         controller.add_cards_to_deck(title)
-        print (controller)
     else:
         choice = input("Would you like to make a new Deck? (Y/N): ")
         while choice != "N":
@@ -30,11 +37,13 @@ def main():
             controller.new_deck(title, desc)
             controller.add_cards_to_deck(title)
             choice = input("Would you like to make a new Deck? (Y/N): ")
-    
-        print (controller)
-        
+
+        name_to_delete = input("Enter deck name to delete it: ")
+        controller.delete_deck(name_to_delete)
+    """
+
     file_sys.write_to_file(controller)
         
         
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
