4e31e19a1ffd08c5c7c2f3bab0114f406f20a8f0
EricJR <ericjohnroe@gmail.com>
2015-12-14 03:19:32 -0800

    THIS IS OUR NEW METHOD OF CALLING THE PROGRAM
    The only files we need to run the program:
    __init__.py (in the same folder as everything else)
    main.py (imports everything else and starts the program
    flashcard_gui.py (previously menucreateview.py, but this name is better)
    flashcard_classes.py (just updated the comments slightly. It still needs a few changes).
    flashcards_file.txt

--- main.py
+++ main.py
@@ -1,49 +1,34 @@
-#TODO: Import all files. Run the app.
+#=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3=========4=========5=========6========7**
+#Course information
+#  Course: CS-362, Software Engineering
+#  Assignment: Final Project
+#  Due date: 2015-Dec-14
+#Project information
+#  Project title: Flash Card Program (aka Never Baguette)
+#  Purpose: There are a multitude of resources available to students to make flash cards, but our goal with Never Baguette was to expand the user’s capabilities. This
+#    program allows those who use it to create flash cards for effective studying.  Users just have to follow simple instructions to create decks of cards. The program
+#    will let them view, edit, and delete decks that they make. It saves paper since everything is done on the computer.  This program will assist countless students on 
+#    their quests for knowledge.
+#  Project files: main.py, flashcard_gui.py, flashcard_classes.py, flashcards_file.txt
+#Module information
+#  Language: Python 3
+#  Date last modified: 2015-Dec-14 (cleaned up some comments)
+#  Purpose: This module calls the program.
+#  File name: main.py
+#  Status: Complete.
+#References and credits
+#  Credits: The internet for its vast knowledge of the library tkinter.
+#Permissions
+#  The source code is free for use by members of the CS-362 class.  Credit this source if you borrow executable statements from this program.  The instructions 
+#  are free to use, but create your own comments.  The comments are intellectual property.
+#
+#===== Begin code area ===================================================================================================================================================
 
-
-
-
-from flashcard_classes import *
-# Testing to make it work in Terminal.
-# Next steps: After this works, we can work on using tkinter with it.
-
-#from library.file_reader import FileSystemStorage
-#from library.flashcard_classes import *
+from flashcard_gui import *
 
 def main():
-    controller = FlashcardController()
-    file_sys = FileSystemStorage()
-    debug = False
-    
-    # Testing the file system
-    file_sys.read_from_file(controller)
-    
-    # Currently debugging and auto-creating some decks
-    """
-    if (debug):
-        title = "Eric's Deck"
-        deck = "This is the description of Eric's Deck."
-        controller.new_deck(title,deck)
-        controller = controller.add_cards_to_deck(title)
-        title = "Deck Dos"
-        desc = "Description Deck Dos, dawg."
-        controller.new_deck(title, desc)
-        controller.add_cards_to_deck(title)
-    else:
-        choice = input("Would you like to make a new Deck? (Y/N): ")
-        while choice != "N":
-            title = input("Enter deck name: ")
-            desc = input("Enter deck description: ")
-            controller.new_deck(title, desc)
-            controller.add_cards_to_deck(title)
-            choice = input("Would you like to make a new Deck? (Y/N): ")
+    create = switchWindow()
+    create.mainloop()
 
-        name_to_delete = input("Enter deck name to delete it: ")
-        controller.delete_deck(name_to_delete)
-    """
-
-    file_sys.write_to_file(controller)
-        
-        
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
